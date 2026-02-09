5ec739b97caf09e45f1f7a2c31f1a76bc46435ba
AlanMonRa <alan_monra@hotmail.com>
2026-02-09 00:22:08 -0600

    feat(Altaalumno):Validaciones dependiendo los días trabajados y mostrando valores según lo ingresado parte 18

--- src/pages/Altaalumno.js
+++ src/pages/Altaalumno.js
@@ -7,10 +7,13 @@ class Altaalumno extends React.Component{
             numeroUno:0,
             numeroDos:0,
             total:0,
+            tipo:'',
+            status:'',
         }
         this.leerNumeroUno = this.leerNumeroUno.bind(this); // Accesamos desde el constructor a nuestra funcion
         this.leerNumeroDos = this.leerNumeroDos.bind(this);
         this.calculaTotal = this.calculaTotal.bind(this);
+        this.ctipo = this.ctipo.bind(this);
     }
     leerNumeroUno (e)
     {
@@ -25,8 +28,27 @@ class Altaalumno extends React.Component{
     }
 
     calculaTotal(e){
-        this.setState({total:parseInt(this.state.numeroUno) + parseInt(this.state.numeroDos)}) // Asignamos el valor a nuestra variable
+        if(this.state.tipo === "A"){
+            this.setState({total:parseInt(this.state.numeroUno) * parseInt(this.state.numeroDos) + 100}) // Asignamos el valor a nuestra variable
+        } else {
+            this.setState({total:parseInt(this.state.numeroUno) * parseInt(this.state.numeroDos) + 400}) // Asignamos el valor a nuestra variable
+        }
+
+        switch(this.state.numeroUno)
+        {
+            case '7':
+                /* return */ this.setState({status:'Buen Empleado'})
+                break;
+            default:
+                /* return */ this.setState({status:'Mal Empleado'})
+                break;
+        }
+
         console.log(this.state.total)
+    }
+
+    ctipo(e){
+        this.setState({tipo:e.target.value})
     }
 
     render(){
@@ -34,17 +56,29 @@ class Altaalumno extends React.Component{
             <div>
                 {/* <form> */}
                     <div>
-                        Ingresa el número 1
-                        <input type='number' name="numeroUno" onChange={this.leerNumeroUno}/>
+                        {/* Ingresa el número 1 */}Días Trabajados
+                        <input type='number' name="numeroUno" onKeyUp={this.leerNumeroUno}/>
                     </div>
                     <div>
-                        Ingresa el número 2
-                        <input type='number' name="numeroDos" onChange={this.leerNumeroDos}/>
+                        {/* Ingresa el número 2 */}Ingreso por día
+                        <input type='number' name="numeroDos" onKeyUp={this.leerNumeroDos}/>
+                    </div>
+                    <div>
+                        Tipo
+                        <select name="Tipo" onChange={this.ctipo}>
+                            <option value='A'>A</option>
+                            <option value='B'>B</option>
+                            <option value='C'>C</option>
+                        </select>
                     </div>
                     <input type="submit" value={"Calculador"} onClick={this.calculaTotal}/>
                     <div>
                         Total
                         <input type='number' name="total" value={this.state.total}/>
+                    </div>
+                    <div>
+                        Status
+                        <input type='text' name="status" value={this.state.status} disabled='disabled'></input>
                     </div>
                 {/* </form> */}
             </div>
