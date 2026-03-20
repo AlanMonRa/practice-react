6bb7a90622c17d0f20101a691ad451d26d4238e9
AlanMonRa <alan_monra@hotmail.com>
2026-03-19 23:20:27 -0600

    feat(InfoAlumno):Se creo la página para los componentes

---
+++ src/pages/InfoAlumno.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AlumnoInfo from "./AlumnoInfo";
+
+class InfoAlumno extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <AlumnoInfo/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default InfoAlumno;
